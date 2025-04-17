5f2d72f08e87738e3511d674afd1d5ae7a6f16dc
Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2025-04-17 21:06:12 +0700

    update

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,8 +1,11 @@
+import { Center, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const HomeComponent = () => {
   return (
-    <div>SELAMAT DATANG DI WEB PESANAN BEBEK QIRUN</div>
+    <Center bg="green" h="100px" color="white">
+      <Text fontWeight={'bold'} align={'center'} textStyle="7xl">SELAMAT DATANG DI WEB PESANAN BEBEK QIRUN. OTEWEEEEEE SABAR BOSS!!!</Text>
+    </Center>
   )
 }
 
